854588505821fc1900d3159258fe9121dc4c53ef
minashaigan <mina.shaigan@gmail.com>
2017-07-19 16:05:57 +0430

    save user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('section');
             $table->string('university');
             $table->string('major');
+            $table->string('data');
             $table->rememberToken();
             $table->timestamps();
         });
