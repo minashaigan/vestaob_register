f49ead41a3d089c8190cf471fc12412db81e9bba
Amin Mohammad <aminmohammadmazreali@gmail.com>
2017-07-20 18:03:15 +0430

    add pad new package

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -3,34 +3,43 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Barryvdh\DomPDF\PDF;
+
 use Illuminate\Http\Request;
-Use PDF;
-use TCPDF_FONTS;
+//Use PDF;
+//use TCPDF_FONTS;
 
 
 class PdfController extends Controller
 {
+
     public function index($email)
     {
-        $user=User::all()->where('email',$email)->first();
+
+
+
+
+        $user=User::where('email',$email)->first();
         if(! is_null($user))
         {
-            $html = '<h1>سلام</h1>';
-            $lg = Array();
-            $lg['a_meta_charset'] = 'UTF-8';
-            $lg['a_meta_dir'] = 'rtl';
-            $lg['a_meta_language'] = 'fa';
-            $lg['w_page'] = 'page';
-            $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/roya.ttf', 'TrueTypeUnicode', '', 120);
-            PDF::SetFont($fontname, '', 2, '', false);
+           // return view('pdf');
 
-// set some language-dependent strings (optional)
-            PDF::setLanguageArray($lg);
-            PDF::SetTitle('Hello World');
-            PDF::AddPage();
-            PDF::writeHTML(view('pdf', ['user'=>$user])->render());
-            PDF::output('resume.pdf', 'I');
-
+            $pdf = \PDF::loadView('pdf');
+            return $pdf->download('resumeee.pdf');
+//            $lg = Array();
+//            $lg['a_meta_charset'] = 'UTF-8';
+//            $lg['a_meta_dir'] = 'rtl';
+//            $lg['a_meta_language'] = 'fa';
+//            $lg['w_page'] = 'page';
+//            $fontname = TCPDF_FONTS::addTTFfont(public_path().'/fonts/roya.ttf', 'TrueTypeUnicode', '', 120);
+//            PDF::SetFont($fontname, '',14, '', false);
+//
+//// set some language-dependent strings (optional)
+//            PDF::setLanguageArray($lg);
+//            PDF::AddPage();
+//            PDF::writeHTML(view('pdf', ['user'=>$user])->render());
+//            PDF::output('resume.pdf', 'I');
+//            return view('pdf');
             //return $user;
 
         }
--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,30 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <title>Bootstrap Example</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <style type="text/css">
-        .test{
-            font-size: 40px;
-            text-align: center;
-        }
-    </style>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8"/>
 </head>
 <body>
-
-<div class="container">
-    <div class="test">
-        <ul>
-
-            <li>{{$user->name}}</li>
-
-
-
-
-        </ul>
-    </div>
-
-
+<p>
+    امین
+</p>
 </body>
 </html>
