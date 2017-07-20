a1bf5f54bc67048c795c3d48cc7085d66c1115b0
Amin Mohammad <aminmohammadmazreali@gmail.com>
2017-07-21 00:03:38 +0430

    edit pdf controller export view

--- resources/views/pdf.blade.php
+++ resources/views/pdf.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html dir="rtl">
 <head>
-    <title>Joe Bloggs - Curriculum Vitae</title>
+    <title>{{$user->name}}</title>
 
     <meta name="viewport" content="width=device-width"/>
     <meta name="description" content="The Curriculum Vitae of Joe Bloggs."/>
