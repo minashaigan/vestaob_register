f7557587e785b59aa251ecd8e658133b3b4e7502
hossein <h.faghihi15@gmail.com>
2017-07-23 23:37:29 +0430

    get fixed

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('/', function () {
 });
 
 Route::post('/user/','InfoController@saveUser');
+Route::get('/user/',function (){
+    return redirect('/formdata');
+});
+
 Route::get('/formdata/','InfoController@data');
 
 Route::get('/pdf/{email}','PdfController@index');
